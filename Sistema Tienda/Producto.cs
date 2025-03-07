d7209642d60215b4a5e590097f57c67c7b22f957
jonixmax <jonixmax@gmail.com>
2025-03-06 20:02:16 -0600

    cambios

--- Sistema Tienda/Producto.cs
+++ Sistema Tienda/Producto.cs
@@ -9,20 +9,20 @@ namespace Sistema_Tienda
     public class Producto
     {
         // Propiedades de la clase Producto
-        public int ID { get; set; } // Identificador único del producto
-        public string Nombre { get; set; } // Nombre del producto
-        public string Categoria { get; set; } // Categoría a la que pertenece el producto
-        public decimal Precio { get; set; } // Precio del producto
-        public int Cantidad { get; set; } // Cantidad disponible en el inventario
+        public int ID { get; set; } 
+        public string Nombre { get; set; } 
+        public string Categoria { get; set; } 
+        public decimal Precio { get; set; } 
+        public int Cantidad { get; set; } 
 
         // Constructor de la clase Producto
         public Producto(int id, string nombre, string categoria, decimal precio, int cantidad)
         {
-            ID = id; // Inicializa el ID del producto
-            Nombre = nombre; // Inicializa el nombre del producto
-            Categoria = categoria; // Inicializa la categoría del producto
-            Precio = precio; // Inicializa el precio del producto
-            Cantidad = cantidad; // Inicializa la cantidad disponible del producto
+            ID = id;
+            Nombre = nombre; 
+            Categoria = categoria; 
+            Precio = precio; 
+            Cantidad = cantidad; 
         }
 
         // Propiedad que retorna una cadena con el nombre del producto, la cantidad y el precio
